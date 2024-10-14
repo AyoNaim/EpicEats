d22d1aa2ed9010fe8ced7eeb314a50ad01f50436
AyoNaim Mide <ayonaim101@gmail.com>
2024-10-14 08:44:07 +0000

    made changes

---
+++ Epiceats/epiceats/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between'>
+        <div>logo</div>
+        <div>links</div>    
+    </nav>
+  )
+}
+
+export default Navbar
